b499904257c17011d5ea3d9a3024f8a1a2db0314
DGgrx <divyanshgarg.divs@gmail.com>
2022-08-10 00:09:38 +0530

    Implemented Splash and Login Screen

---
+++ lib/screens/authenticate/sign_in.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class SignIn extends StatefulWidget {
+  const SignIn({Key? key}) : super(key: key);
+
+  @override
+  State<SignIn> createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+  final _formKey = GlobalKey<FormState>();
+  String? mobileNo;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(28.0),
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            SizedBox(
+              height: 150,
+            ),
+            Text(
+              "India's #1 Satsang Streaming App",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 35.0,
+              ),
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            Text(
+              "Insert Your mobile Number",
+              textAlign: TextAlign.start,
+              style: TextStyle(
+                fontSize: 20.0,
+              ),
+            ),
+            SizedBox(
+              height: 14,
+            ),
+            Expanded(
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        Text(
+                          "+91",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 28.0),
+                        ),
+                        SizedBox(
+                          width: 10.0,
+                        ),
+                        Expanded(
+                            child: TextFormField(
+                          validator: (value)=> value!.isNotEmpty && value.length == 10
+                              ?null
+                              :"Please Enter a Mobile Number",
+                          onChanged: (value) =>
+                              setState(() => mobileNo = value),
+                          style: TextStyle(fontSize: 25.0, letterSpacing: 1.0),
+                          maxLength: 10,
+                          maxLengthEnforcement: MaxLengthEnforcement.enforced,
+                          // validator: ,
+                          decoration: InputDecoration(
+                              counterText: "",
+                              contentPadding: EdgeInsets.symmetric(
+                                  vertical: 0, horizontal: 15.0),
+                              hintText: "Mobile Number",
+                              hintStyle: TextStyle(fontSize: 17.0),
+                              border: OutlineInputBorder(
+                                  borderSide: BorderSide(width: 1.0))),
+                          keyboardType: TextInputType.number,
+                          inputFormatters: <TextInputFormatter>[
+                            FilteringTextInputFormatter.digitsOnly
+                          ],
+                        )),
+                      ],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 35.0, 0, 0),
+                      child: ElevatedButton(
+                          onPressed: () {
+                            if(_formKey.currentState!.validate()){
+                                print(mobileNo.runtimeType);
+                            }
+                          },
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(
+                                vertical: 12.0, horizontal: 8.0),
+                            child: Text(
+                              "Get OTP",
+                              style: TextStyle(fontSize: 19.0),
+                            ),
+                          )),
+                    )
+                  ],
+                ),
+              ),
+            )
+          ]),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:satsang_app/screens/authenticate/sign_in.dart';
+
+class Wrapper extends StatelessWidget {
+  const Wrapper({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SignIn();
+  }
+}
